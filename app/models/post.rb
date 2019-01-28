08514d7cb60e94fb49900e225d016f48d4b564da
Uuya <a1b23cd@icloud.com>
2019-01-28 19:02:26 +0900

    Add  association user & add validates presence true

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  validates :title,          presence: true
+  validates :studied_at,     presence: true
+  validates :study_time,     presence: true
+  validates :content,        presence: true
+  validates :study_method,   presence: true
 end
