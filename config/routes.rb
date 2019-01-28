75c84b2591e5925586033b1b53183d77bff48b3d
Uuya <a1b23cd@icloud.com>
2019-01-28 19:01:12 +0900

    Add routes posts show, new, create, edit, update, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  resources :posts, only: [:show, :new, :create, :edit, :update, :destroy]
+
 end
