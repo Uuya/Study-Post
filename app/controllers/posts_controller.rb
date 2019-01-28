3e889084c7ddeb2d78f865d2468e5f99fb09165e
Uuya <a1b23cd@icloud.com>
2019-01-28 19:03:41 +0900

    Add posts controller action show, new, create, edit, update, destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,33 @@
 class PostsController < ApplicationController
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :studied_at, :study_time, :content, :study_method, :reflection, :next_study, :other).merge(user_id: current_user.id)
+  end
+
 end
+
+
