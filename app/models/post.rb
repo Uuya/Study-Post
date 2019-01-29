bb4c6683b9a2932c113b04336f6f687415eaf824
Uuya <a1b23cd@icloud.com>
2019-01-29 20:50:41 +0900

    Add validate post title length & studied_at uniqueness

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :title,          presence: true
-  validates :studied_at,     presence: true
+  validates :title,          presence: true, length: { maximum: 20 }
+  validates :studied_at,     presence: true, uniqueness: true
   validates :study_time,     presence: true
   validates :content,        presence: true
   validates :study_method,   presence: true
