70cc7372a341aba657e21f501246b2b658c8e568
Uuya <a1b23cd@icloud.com>
2019-01-29 20:50:59 +0900

    Add validate user name uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 8 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 8 }
 
   has_many :posts, dependent: :destroy
 end
