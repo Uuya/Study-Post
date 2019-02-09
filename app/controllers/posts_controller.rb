557e567177362f456f9e02bac656f743948d025c
Uuya <a1b23cd@icloud.com>
2019-02-09 21:09:02 +0900

    Fixed posts controller Add flash message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,9 +7,10 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to user_path(@post.user_id)
+      redirect_to user_path(@post.user_id), success: '投稿しました'
     else
-      redirect_to new_post_path
+      flash.now[:error] = '投稿に失敗しました。'
+      render new_post_path
     end
   end
 
@@ -22,15 +23,23 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(id_params[:id])
-      post.update(post_params)
-      redirect_to "/posts/#{post.id}", notice: 'ブログを編集しました'
+    @post = Post.find(id_params[:id])
+      if @post.update(post_params)
+        redirect_to "/posts/#{@post.id}", success: '編集しました'
+      else
+        flash.now[:error] = '編集に失敗しました。'
+        render :edit
+      end
   end
 
   def destroy
     post = Post.find(id_params[:id])
-    post.destroy
-    redirect_to user_path(post.user_id)
+    if post.destroy
+    redirect_to user_path(post.user_id), success: '削除しました'
+    else
+      flash.now[:error] = '削除に失敗しました。'
+      render :show
+    end
   end
 
 
