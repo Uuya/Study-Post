91b094109b5534bdc82a70658d55d42d2c064c7f
Uuya <a1b23cd@icloud.com>
2019-02-12 21:45:47 +0900

    Change column default users avatar nil

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_122036) do
+ActiveRecord::Schema.define(version: 2019_02_12_122750) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2019_02_11_122036) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "avatar", default: "no-image.png"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
