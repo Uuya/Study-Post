d2b099880f2d00fe56df473938691166d2160648
Uuya <a1b23cd@icloud.com>
2019-02-15 12:13:30 +0900

    Add user relationship follow relation & add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,24 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 8 }
 
   has_many :posts, dependent: :destroy
+  has_many :active_relationships,class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships,class_name:  "Relationship", foreign_key: "following_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :following
+  has_many :followers, through: :passive_relationships, source: :follower
+
+   # ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(following_id: other_user.id)
+  end
+
+  # ユーザーをアンフォローする
+  def unfollow(other_user)
+    active_relationships.find_by(following_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 end
