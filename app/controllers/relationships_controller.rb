c47296e99c7199d121b3360c17e89387f2e84447
Uuya <a1b23cd@icloud.com>
2019-02-15 12:13:48 +0900

    Fixed Relationships Controller Add follow & unfollow function

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,17 @@
+class RelationshipsController < ApplicationController
+  def create
+    relationship =  Relationship.create(create_params)
+    redirect_to user_path(id: relationship[:following_id])
+  end
+
+  def destroy
+    relationship =  Relationship.find(params[:id])
+    relationship.destroy
+    redirect_to user_path(id: relationship[:following_id])
+  end
+
+  private
+  def create_params
+    params.permit(:following_id).merge(follower_id: current_user.id)
+  end
+end
