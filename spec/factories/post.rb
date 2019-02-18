808c258da2e47f19157760e613922a3ecf759ce3
Uuya <a1b23cd@icloud.com>
2019-02-18 23:56:45 +0900

    Bundle update Changes in the new version

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :post do
-      title           'テストタイトル'
-      studied_at      Faker::Number.number(8)
-      study_time      Faker::Number.between(0, 12)
-      study_minutes   Faker::Number.between(0, 59)
-      content         'テスト内容'
-      study_method    'テスト勉強方法'
-      created_at      Faker::Time.between(2.days.ago, Time.now, :all)
-      updated_at      Faker::Time.between(2.days.ago, Time.now, :all)
+      title           {'テストタイトル'}
+      studied_at      {Faker::Number.number(8)}
+      study_time      {Faker::Number.between(0, 12)}
+      study_minutes   {Faker::Number.between(0, 59)}
+      content         {'テスト内容'}
+      study_method    {'テスト勉強方法'}
+      created_at      {Faker::Time.between(2.days.ago, Time.now, :all)}
+      updated_at      {Faker::Time.between(2.days.ago, Time.now, :all)}
       user
   end
 end
