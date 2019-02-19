9da2cd48b6d35c8cc179adafcd09cee48154fd93
Uuya <a1b23cd@icloud.com>
2019-02-19 20:23:45 +0900

    Change column posts study_time change column type time to integer

--- db/migrate/20190128080329_change_column_to_posts.rb
+++ db/migrate/20190128080329_change_column_to_posts.rb
@@ -1,6 +1,6 @@
 class ChangeColumnToPosts < ActiveRecord::Migration[5.2]
   def change
     change_column :posts, :studied_at, :integer
-    change_column :posts, :study_time, :time
+    change_column :posts, :study_time, :integer
   end
 end
