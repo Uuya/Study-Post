aa7852eb57c9a1935083331f0a3a761534a92f96
Uuya <a1b23cd@icloud.com>
2019-02-27 10:00:15 +0900

    Fixed post column validates change uniqueness: true to uniqueness: { scope: [:user_id] }

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :title,          presence: true, length: { maximum: 20 }
-  validates :learning_date,     presence: true, uniqueness: true, length: { is: 8, allow_blank: true }
-  validates :study_time,     presence: true
-  validates :study_minutes,  presence: true
-  validates :content,        presence: true
-  validates :study_method,   presence: true
+  validates  :title,         presence: true, length: { maximum: 20 }
+  validates  :learning_date, presence: true, uniqueness: { scope: [:user_id] }, length: { is: 8, allow_blank: true }
+  validates  :study_time,    presence: true
+  validates  :study_minutes, presence: true
+  validates  :content,       presence: true
+  validates  :study_method,  presence: true
 end
