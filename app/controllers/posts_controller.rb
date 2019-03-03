7671d6204616d497003c048673ce35fc2d7f3bd4
Uuya <a1b23cd@icloud.com>
2019-03-03 17:03:16 +0900

    Fixed posts controller edit message & add user check function edit, update, destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user,   only: [:edit, :update, :destroy]
+  include PostsHelper
 
   def new
     @post = Post.new
@@ -8,9 +10,9 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to user_path(@post.user_id), success: '投稿しました'
+      redirect_to user_path(@post.user_id), success: '投稿しました！'
     else
-      flash.now[:error] = '投稿に失敗しました。'
+      flash.now[:error] = '投稿に失敗しました'
       render new_post_path
     end
   end
@@ -25,24 +27,23 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(id_params[:id])
-      if @post.update(post_params)
-        redirect_to "/posts/#{@post.id}", success: '編集しました'
-      else
-        flash.now[:error] = '編集に失敗しました。'
-        render :edit
-      end
+    if @post.update(post_params)
+      redirect_to "/posts/#{@post.id}", success: '編集しました！'
+    else
+      flash.now[:error] = '編集に失敗しました'
+      render :edit
+    end
   end
 
   def destroy
     post = Post.find(id_params[:id])
     if post.destroy
-      redirect_to user_path(post.user_id), success: '投稿を削除しました'
+      redirect_to user_path(post.user_id), success: '投稿を削除しました！'
     else
-      flash.now[:error] = '削除に失敗しました。'
+      flash.now[:error] = '削除に失敗しました'
       render :show
     end
   end
-
 
   private
 
@@ -53,6 +54,13 @@ class PostsController < ApplicationController
   def id_params
     params.permit(:id)
   end
+
+  def correct_user
+    post  = Post.find(id_params[:id])
+    @user = post.user_id
+    unless current_user?(@user)
+      flash[:error] = "操作が受け付けられません"
+      redirect_to(user_path)
+    end
+  end
 end
-
-
